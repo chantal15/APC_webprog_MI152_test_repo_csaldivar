777fbf0b93c586982628e12e7bfb3cc7e4f6f3e0
chantal15 <chantalsaldivar530@gmail.com>
2016-11-19 22:47:50 +0800

    Update index.php
    Fixing error

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -4,7 +4,7 @@ include_once 'dbconfig.php';
 // delete condition
 if(isset($_GET['delete_id']))
 {
- $sql_query="DELETE FROM users WHERE user_id=".$_GET['get_id'];
+ $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
  mysql_query($sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
