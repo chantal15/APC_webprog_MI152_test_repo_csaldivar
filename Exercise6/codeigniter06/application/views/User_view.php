4d0f97255d91332264982a6869ebd5f2349241b0
chantal15 <chantalsaldivar530@gmail.com>
2016-11-22 22:47:54 +0800

    Create view file called User_view.php

---
+++ Exercise6/codeigniter06/application/views/User_view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html> 
+<html lang = "en">
+ 
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>Students Example</title> 
+   </head>
+	
+   <body> 
+      <a href = "<?php echo base_url(); ?>index.php/user/add_view">Add</a>
+		
+      <table border = "1"> 
+         <?php 
+            $i = 1; 
+            echo "<tr>"; 
+            echo "<td>Sr#</td>";
+            echo "<td>User No.</td>";  
+            echo "<td>First Name</td>";
+            echo "<td>Last Name</td>";
+            echo "<td>Middle Name</td>";
+            echo "<td>Nickname</td>";
+            echo "<td>Email Address</td>";
+            echo "<td>Home Address</td>";
+            echo "<td>Gender</td>";       
+            echo "<td>Cellphone no.</td>"; 
+            echo "<td>Edit</td>"; 
+            echo "<td>Delete</td>"; 
+            echo "<tr>"; 
+				
+            foreach($records as $r) { 
+               echo "<tr>"; 
+               echo "<td>".$i++."</td>";
+               echo "<td>".$r->user_id."</td>";
+               echo "<td>".$r->firstname."</td>";
+               echo "<td>".$r->lastname."</td>";
+               echo "<td>".$r->midname."</td>";
+               echo "<td>".$r->nickname."</td>";
+               echo "<td>".$r->email."</td>";
+               echo "<td>".$r->homeadd."</td>";
+               echo "<td>".$r->gender."</td>";
+               echo "<td>".$r->cellno."</td>"; 
+               echo "<td><a href = '".base_url()."index.php/user/edit/"
+                  .$r->user_id."'>Edit</a></td>"; 
+               echo "<td><a href = '".base_url()."index.php/user/delete/"
+                  .$r->user_id."'>Delete</a></td>"; 
+               echo "<tr>"; 
+            } 
+         ?>
+      </table> 
+		
+   </body>
+	
+</html>
+
