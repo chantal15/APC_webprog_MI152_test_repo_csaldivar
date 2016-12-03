02ed00ccfd79b2b9c6c7f3860b47ab522057b5d9
chantal15 <chantalsaldivar530@gmail.com>
2016-12-03 19:19:32 +0800

    changing base url

--- Exercise6/codeigniter3.0/application/config/config.php
+++ Exercise6/codeigniter3.0/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/codeigniter3.0/';
+$config['base_url'] = 'http://127.0.0.1/exercise6/codeigniter3.0/';
 
 /*
 |--------------------------------------------------------------------------
