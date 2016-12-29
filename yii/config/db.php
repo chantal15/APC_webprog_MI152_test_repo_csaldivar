04a7439b83e0b6be15228b285cf7253e83cf8c3b
chantal15 <chantalsaldivar530@gmail.com>
2016-12-30 00:19:06 +0800

    Changing password to numbers

--- yii/config/db.php
+++ yii/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ohmy',
     'username' => 'root',
-    'password' => 'chanty',
+    'password' => '09479',
     'charset' => 'utf8',
 ];
